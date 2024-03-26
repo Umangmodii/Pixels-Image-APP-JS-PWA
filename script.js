f3b9e78734a45a1c65ef362fd476b5bd501621f9
umangmodii <Umangmodi003@gmail.com>
2024-03-27 01:26:24 +0530

    commit

---
+++ script.js
@@ -0,0 +1,88 @@
+const apiKey = 'mJStW8Y04RIxhcOsArZneoFnUgoVJiD1CeD2lZgQI1v3msZxjDISfsZE';
+let currentPage = 1;
+let totalPages = 1;
+let totalResults = 0;
+
+function searchImages(query, page) {
+  let apiUrl = `https://api.pexels.com/v1/curated?per_page=20&page=${page}`;
+  if (query) {
+    apiUrl = `https://api.pexels.com/v1/search?query=${query}&per_page=20&page=${page}`;
+  }
+
+  fetch(apiUrl, {
+    headers: {
+      Authorization: apiKey,
+    },
+  })
+    .then(response => response.json())
+    .then(data => {
+      const imagesContainer = document.getElementById('imagesContainer');
+      imagesContainer.innerHTML = ''; // Clear previous search results
+
+      // Create rows for images
+      for (let i = 0; i < data.photos.length; i += 4) {
+        const row = document.createElement('div');
+        row.classList.add('row', 'justify-content-center');
+
+        for (let j = i; j < i + 4 && j < data.photos.length; j++) {
+          const imgElement = document.createElement('img');
+          imgElement.src = data.photos[j].src.medium;
+          imgElement.alt = data.photos[j].alt;
+          imgElement.classList.add('img-fluid', 'rounded', 'm-2', 'col-md-3'); // Adjust column size for 4 images
+          row.appendChild(imgElement);
+        }
+
+        imagesContainer.appendChild(row);
+      }
+
+      // Update total results
+      totalResults = data.total_results;
+      updateTotalResults();
+
+      // Update pagination
+      currentPage = page;
+      totalPages = Math.ceil(data.total_results / 20);
+      updatePaginationButtons();
+    })
+    .catch(error => console.error('Error fetching images:', error));
+}
+
+function updateTotalResults() {
+  const totalResultsElement = document.getElementById('totalResults');
+  totalResultsElement.textContent = `Total Results: ${totalResults}`;
+}
+
+function updatePaginationButtons() {
+  const prevPageBtn = document.getElementById('prevPage');
+  const nextPageBtn = document.getElementById('nextPage');
+
+prevPageBtn.disabled = currentPage === 1;
+nextPageBtn.disabled = currentPage === totalPages;
+
+prevPageBtn.addEventListener('click', () => {
+if (currentPage > 1) {
+searchImages(document.getElementById('searchInput').value.trim(), currentPage - 1);
+}
+});
+
+nextPageBtn.addEventListener('click', () => {
+if (currentPage < totalPages) {
+searchImages(document.getElementById('searchInput').value.trim(), currentPage + 1);
+}
+});
+}
+
+document.getElementById('searchBtn').addEventListener('click', () => {
+const searchInput = document.getElementById('searchInput');
+const query = searchInput.value.trim();
+if (query !== '') {
+searchImages(query, 1);
+} else {
+alert('Please enter search keywords.');
+}
+});
+
+// Fetch and display images on page load
+document.addEventListener('DOMContentLoaded', () => {
+searchImages('', 1); // Empty query fetches 20 random images
+});
